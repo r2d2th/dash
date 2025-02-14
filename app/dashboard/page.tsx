c92899de356e92990be9ee7fbec91efe61e16b01
r2d2th <aandb.tg@gmail.com>
2025-02-14 20:46:06 +0200

    chore

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,3 @@
-// export default function Page() {
-//   return <p>Dashboard Page</p>;
-// }
-
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
